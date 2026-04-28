7facf193a49fe099a9f3653a4b4524c489aa5fd1
dev-mofazzal <dev.mofazzalhossain@gmail.com>
2026-04-28 23:57:51 +0600

    Svg animation with gsap

--- main.js
+++ main.js
@@ -1,62 +1,22 @@
-gsap.from('#page1 #box',{
-    scale: 0,
-    opacity: 0,
-    duration: 1,
-    delay: .5,
-    rotate: 360,
+var initialPath = 'M 0 100 Q 250 100 1500 100';
+var finalPath = 'M 0 100 Q 250 100 1500 100';
+
+var containerEle = document.querySelector('.container');
+
+containerEle.addEventListener('mousemove', function (ele) {
+    path = `M 0 100 Q ${ele.x} ${ele.y} 1500 100`;
+    console.log(path);
+    gsap.to('.container path', {
+        attr: { d: path },
+        duration: .3,
+        ease: "power3.out",
+    })
 });
 
-gsap.from("#page2 #box",{
-    scale: 0,
-    duration: 1,
-    rotate: 360,
-    scrollTrigger:{
-        trigger: "#page2 #box",
-        scroller: "body",
-        markers: true, 
-        start: "top 50%",
-        scrub: 3
-    }
-});
-
-gsap.from("#page2 h1", {
-    x: 500,
-    duration: 2,
-    opacity: 0,
-    scrollTrigger: {
-        trigger: "#page2 h1",
-        scroll: "body",
-        markers: 1,
-        start: "top 50%",
-        end: "top 20%",
-        scrub: 3,
-    }
-});
-
-gsap.from("#page2 h2", {
-    x: -500,
-    duration: 2,
-    opacity: 0,
-    scrollTrigger: {
-        trigger: "#page2 h2",
-        markers: true,
-        start: "top 50%",
-        end: "top 20%",
-        scroll: 'body',
-        scrub: 3,
-    }
-})
-
-
-gsap.to("#page3 h2", {
-    transform: "translateX(-57%)",
-    scrollTrigger:{
-        trigger: "#page3",
-        scroll: 'body',
-        markers: true,
-        pin: true,
-        start: 'top 0%',
-        end: 'top -200%',
-        scrub: 5,
-    }
+containerEle.addEventListener('mouseleave', function () {
+    gsap.to('.container path', {
+        attr: { d: finalPath },
+        ease: "elastic.out(1,0.1)",
+        duration: 1.5,
+    })
 })
